acb252bf5242a7fd7623bc4db22c1ee261e1d7ea
YashRawat <139982135+YashRawat911@users.noreply.github.com>
2023-09-22 12:43:27 +0530

    Update g.sh

--- g.sh
+++ g.sh
@@ -1,4 +1,4 @@
-#wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz
+wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz
 
 #tar -xf xmrig-6.20.0-linux-x64.tar.gz
 
